718527e868bf9ff165fb624e9603a8be7149cf6f
TejasTS2006 <tv03082@gmail.com>
2026-02-08 21:20:20 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        // The line below prints "Hello, World!" to the console and moves to the next line.
+        System.out.println("Hello, World!");
+    }
+}
